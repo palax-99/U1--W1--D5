8eeb8a58a5d73cbec28f83adc6fda9e6f2b7442f
palax-99 <ninociao7@gmail.com>
2026-03-13 15:27:20 +0100

    implements the class Image
    a differenza delle altre due classi, utilizzo solo una variabile, perché l'altra la eredito dalla classe padre, e non uso play, ma un metodo esclusivo solo per la classe immagine, ovvero show

--- src/entites/Immagine.java
+++ src/entites/Immagine.java
@@ -1,4 +1,36 @@
 package entites;
 
 public class Immagine extends ElementoMultimediale {
+    private int brightness;
+    // A differenza delle altre due classi figlie, necessitò solo della variabile brightness(luminosità)
+    // In quanto titolo, la eredito sempre dal padre
+
+    public Immagine(String titolo, int brightness) {
+        super(titolo);
+        this.brightness = brightness;
+    }
+
+    public int increaseBrightness() {
+        return brightness++;
+    }
+
+    public int decreasesBrightness() {
+        if (brightness > 0) {
+            return brightness--;
+        }
+        return brightness;
+    }
+
+    public void show() {
+        // Metodo specifico della classe Immagine.
+        // Non uso play() perché le immagini non sono riproducibili.
+
+        System.out.print(titolo);
+
+        for (int contatore = 0; contatore < brightness; contatore++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
 }
