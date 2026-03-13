891973ef0e590afd36960cb8cf0a2b6ff136b182
palax-99 <ninociao7@gmail.com>
2026-03-13 15:01:44 +0100

    implements the class Video
    a differenza della classe Audio, in Video, ho aggiunto una nuova variabile, che ho passato nel costruttore Video come parametro, scelta a mio parere più sensata per la consegna dell'esercizio, a cui ho aggiunto, due ulteriori metodi

--- src/entites/Audio.java
+++ src/entites/Audio.java
@@ -5,7 +5,7 @@ import interfaces.Riproducibile;
 public class Audio extends ElementoMultimediale implements Riproducibile {
     // ho impostato il codice in modo tale che Audio fosse una sottoclasse di ElementoMultimediale e al contempo implementi anche l'interfaccia Riproducibile
     private int duration; // creo una variabile private durata(Best Practice)
-    private int quantity //volume;
+    private int quantity; //volume;
 
     public Audio(String titolo, int duration, int quantity) {
         super(titolo); // eredita il titolo dal padre ElementoMultimediale
@@ -14,7 +14,7 @@ public class Audio extends ElementoMultimediale implements Riproducibile {
     }
 
     public int turnUpTheQuantity() {  // creo un metodo che mi permette di alzare il volume
-       return quantity++;
+        return quantity++;
     }
 
     public int turnDownTheQuantity() { // creo un metodo che abbassa il volume
--- src/entites/Video.java
+++ src/entites/Video.java
@@ -1,4 +1,59 @@
 package entites;
 
-public class Video extends ElementoMultimediale {
+import interfaces.Riproducibile;
+
+public class Video extends ElementoMultimediale implements Riproducibile {
+    private int duration;
+    private int quantity;
+    private int brightness; // aggiungo variabile per consegna dell'esercizio
+
+    public Video(String titolo, int duration, int quantity, int brightness) {
+        // per la classe video opto per un costruttore a cui, a differenza di Audio, gli passo quattro parametri
+        super(titolo); // elemento sempre ereditato dalla classe padre
+        this.duration = duration;
+        this.quantity = quantity;
+        this.brightness = brightness;
+    }
+
+    public int turnUpTheQuantity() {
+        return quantity++;
+    }
+
+    public int turnDownTheQuantity() {
+        if (quantity > 0) {
+            quantity--;
+        }
+        return quantity;
+    }
+
+    public int increaseBrightness() {
+        // Differenza dalla classe Audio, creo altri due metodi per decidere se aumentare o dimuire la luminosità
+        return brightness++;
+    }
+
+    public int decreasesBrightness() {
+        if (brightness > 0) {
+            return brightness--;
+        }
+        return brightness;
+    }
+
+    public void play() {
+
+        for (int contatore = 0; contatore < duration; contatore++) {
+
+            System.out.print(titolo);
+
+            for (int contatorej = 0; contatorej < quantity; contatorej++) {
+                System.out.print("!");
+            }
+
+            for (int contatorez = 0; contatorez < brightness; contatorez++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
 }
